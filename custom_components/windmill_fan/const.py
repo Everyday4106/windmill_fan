fcd41e71d77d51069bb12dde2eb819f34c0be311
Everyday4106 <167035046+Everyday4106@users.noreply.github.com>
2024-11-01 08:11:22 -0400

    Update const.py

--- custom_components/windmill_fan/const.py
+++ custom_components/windmill_fan/const.py
@@ -1,10 +1,10 @@
-"""Constants for Windmill Air Fan   ."""
+"""Constants for Windmill Fan."""
 from logging import Logger, getLogger
 
 LOGGER: Logger = getLogger(__package__)
 
-NAME = "WindmillAirFan"
-DOMAIN = "windmillairfan"
+NAME = "Windmill Fan"
+DOMAIN = "windmill_fan"
 VERSION = "1.0.0"
 PLATFORMS = ["fan"]
 UPDATE_INTERVAL = 60
