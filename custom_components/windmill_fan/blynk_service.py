6630961c8e635822581ef1ce8247fcd8ca38d940
Everyday4106 <167035046+Everyday4106@users.noreply.github.com>
2024-11-01 08:07:02 -0400

    Update blynk_service.py

--- custom_components/windmill_fan/blynk_service.py
+++ custom_components/windmill_fan/blynk_service.py
@@ -10,7 +10,7 @@ class BlynkService:
         self.hass = hass
         self.server = server
         self.token = token
-        # Mapping of string values to pin values
+        
         self.power_mapping = {
             False: 0,
             True: 1
@@ -32,18 +32,15 @@ class BlynkService:
         return f"{self.server}/{endpoint}?{query}"
 
     async def async_get_pin_value(self, pin):
-        _LOGGER.debug(f"Getting pin value for pin {pin}")
+        _LOGGER.debug(f"Getting value for pin {pin}")
         params = {'token': self.token}
         url = self._get_request_url(f'external/api/get', params) + f"&{pin}"
         _LOGGER.debug(f"Request URL: {url}")
 
         def fetch():
             response = requests.get(url)
-            _LOGGER.debug(f"Response Status Code: {response.status_code}")
-            _LOGGER.debug(f"Response Text: {response.text}")
 
             if response.status_code == 200:
-                _LOGGER.debug(f"Response RAW: {response}")
                 try:
                     if response.text.isdigit():
                         return int(response.text)
@@ -54,7 +51,7 @@ class BlynkService:
                 except (ValueError, IndexError) as e:
                     raise Exception(f"Failed to parse response for {pin}: {e}")
             else:
-                raise Exception(f"Failed to get pin value for {pin}")
+                raise Exception(f"Failed to get value for pin {pin}")
 
         return await self.hass.async_add_executor_job(fetch)
 
@@ -72,57 +69,53 @@ class BlynkService:
             if response.status_code == 200:
                 return response.text.strip()
             else:
-                raise Exception(f"Failed to set pin value for {pin}")
+                raise Exception(f"Failed to set value for pin {pin}")
 
         return await self.hass.async_add_executor_job(fetch)
 
     async def async_set_power(self, value):
-        _LOGGER.debug(f"Setting Raw Power: {value}")
         pin_value = self.power_mapping.get(value, "0")
-        _LOGGER.debug(f"Setting Power: {pin_value}")
+        _LOGGER.debug(f"Setting Power pin to {pin_value}")
         await self.async_set_pin_value('V0', pin_value)
 
     async def async_get_power(self) -> bool:
         pin_value = await self.async_get_pin_value('V0')
-        _LOGGER.debug(f"Pin value received for power: {pin_value} (type: {type(pin_value)})")
+        _LOGGER.debug(f"The power pin ({pin_value}) is set to {type(pin_value)}")
         if pin_value == 1:
             return True
         else:
             return False
 
     async def async_set_autofade(self, value):
-        _LOGGER.debug(f"Setting Raw Power: {value}")
         pin_value = self.power_mapping.get(value, "0")
-        _LOGGER.debug(f"Setting Power: {pin_value}")
+        _LOGGER.debug(f"Setting AutoFade pin to {pin_value}")
         await self.async_set_pin_value('V1', pin_value)
 
     async def async_get_autofade(self) -> bool:
         pin_value = await self.async_get_pin_value('V1')
-        _LOGGER.debug(f"Pin value received for autofade: {pin_value} (type: {type(pin_value)})")
+        _LOGGER.debug(f"The Autofade pin ({pin_value}) is set to {type(pin_value)}")
         if pin_value == 1:
             return True
         else:
             return False
 
     async def async_set_speed(self, value):
-        # value = value.lower()  # Ensure consistent casing
         pin_value = self.speed_mapping.get(value, "0")
-        _LOGGER.debug(f"Setting speed {value} to pin {pin_value}")
+        _LOGGER.debug(f"Setting pin {pin_value} to speed {value}")
         await self.async_set_pin_value('V2', pin_value)
 
     async def async_get_speed(self):
         pin_value = await self.async_get_pin_value('V2')
-        pin_value = str(pin_value).lower()
         speed = "Speed 1"
         
         if pin_value == 2:
             speed = "Speed 2"
-        if pin_value == 3:
+        elif pin_value == 3:
             speed = "Speed 3"
-        if pin_value == 4:
+        elif pin_value == 4:
             speed = "Speed 4"
-        if pin_value == 5:
+        elif pin_value == 5:
             speed = "Speed 5"
 
-        _LOGGER.debug(f"Pin value: {pin_value} is mapped mode mapped to: {speed}")
+        _LOGGER.debug(f"Pin ({pin_value}) is set to speed {speed}")
         return speed
