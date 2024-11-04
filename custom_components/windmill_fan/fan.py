9f020fa7664ff108a3bce271cf32764fe95f578c
Everyday4106 <167035046+Everyday4106@users.noreply.github.com>
2024-11-04 08:14:29 -0500

    Update fan.py

--- custom_components/windmill_fan/fan.py
+++ custom_components/windmill_fan/fan.py
@@ -1,35 +1,33 @@
 import logging
-from homeassistant.components.fan import FanEntity
-from homeassistant.helpers.entity import DeviceInfo
+
+from homeassistant.components.fan import FanEntityDescription
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import DOMAIN
+from .coordinator import WindmillDataUpdateCoordinator
+from .entity import WindmillFan
 
 _LOGGER = logging.getLogger(__name__)
 _LOGGER.setLevel(logging.DEBUG)
 
-class WindmillFan(FanEntity):
-  _attr_has_entity_name = True
-  _attr_name = None
-  
-  def __init__(self):
-    _LOGGER.debug(f"setting up the fan entity...")
-    self._is_on = False
-    self._attr_device_info = DeviceInfo(
-      identifiers={(DOMAIN, self.unique_id)},
-      name= "Windmill Fan",
-      manufacturer="Windmill"
-    )
-    self._attr_unique_id = f"{DOMAIN}_{entity_description.key}"
-    
-    @property
-    def is_on(self):
-      """If the fan is currently on or off."""
-      return self._is_on
-    
-    def turn_on(self, **kwargs):
-      """Turn the fan on."""
-      self._is_on = True
+ENTITY_DESCRIPTIONS = [
+    FanEntityDescription(
+        key="windmill_fan",
+        name="Windmill fan",
+        icon="mdi:air-conditioner",
+    ),
+]
 
-    def turn_off(self, **kwargs):
-      """Turn the fan off."""
-      self._is_on = False
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback) -> None:
+    """Set up the Windmill Fan fan entity."""
+    coordinator = hass.data[DOMAIN]["coordinator"]
+
+    async_add_entities(
+        WindmillFan(
+            coordinator=coordinator,
+            entity_description=entity_description,
+        )
+        for entity_description in ENTITY_DESCRIPTIONS
+    )
