c3e71de613c8e2c74eeebc168274d22f393c930d
Everyday4106 <167035046+Everyday4106@users.noreply.github.com>
2024-11-06 18:41:09 -0500

    Update __init__.py

--- custom_components/windmill_fan/__init__.py
+++ custom_components/windmill_fan/__init__.py
@@ -37,37 +37,4 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     if unload_ok:
         hass.data[DOMAIN].pop("coordinator")
 
-    return unload_okimport logging
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
-
-from .const import DOMAIN, PLATFORMS, CONF_TOKEN, BASE_URL
-from .blynk_service import BlynkService
-from .coordinator import WindmillDataUpdateCoordinator
-from .fan import WindmillFan
-
-_LOGGER = logging.getLogger(__name__)
-_LOGGER.setLevel(logging.DEBUG)
-
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    _LOGGER.debug("Setting up Windmill Fan config entry")
-
-    server = BASE_URL
-    token = entry.data[CONF_TOKEN]
-    
-    blynk_service = BlynkService(hass, server, token)
-    coordinator = WindmillDataUpdateCoordinator(hass, blynk_service)
-
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN]["coordinator"] = coordinator
-    
-    # Fetch the current values for the fan
-    #await coordinator.async_config_entry_first_refresh()
-
-    #await blynk_service.async_set_power(1)
-    
-    #async_add_entities(
-    #    WindmillFan(coordinator, idx) for idx, ent in enumerate(coordinator.data)
-    #)
-    
-    return True
+    return unload_ok
