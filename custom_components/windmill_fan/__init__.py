267cd30366a20274742fbf71b6f74d08d8283559
Everyday4106 <167035046+Everyday4106@users.noreply.github.com>
2024-11-06 13:45:52 -0500

    Update __init__.py

--- custom_components/windmill_fan/__init__.py
+++ custom_components/windmill_fan/__init__.py
@@ -25,7 +25,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     # Fetch the current values for the fan
     await coordinator.async_config_entry_first_refresh()
 
-    await blynk_service.async_set_power(1)
+    #await blynk_service.async_set_power(1)
     
     #async_add_entities(
     #    WindmillFan(coordinator, idx) for idx, ent in enumerate(coordinator.data)
