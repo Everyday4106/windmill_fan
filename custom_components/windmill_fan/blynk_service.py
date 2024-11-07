250161442c3fb55e1a5dd0a127b46dcb5052be5e
Everyday4106 <167035046+Everyday4106@users.noreply.github.com>
2024-11-07 09:57:25 -0500

    Update blynk_service.py

--- custom_components/windmill_fan/blynk_service.py
+++ custom_components/windmill_fan/blynk_service.py
@@ -74,6 +74,8 @@ class BlynkService:
         _LOGGER.debug(f"Pin value received for power: {pin_value}")
         _LOGGER.debug(self.power_mapping.keys())
         _LOGGER.debug(self.power_mapping.values())
+        value = {i for i in self.power_mapping if self.power_mapping[i]==1}
+        _LOGGER.debug(value)
         if pin_value == 1:
             return "On"
         else:
