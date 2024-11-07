b65974ca5fcc069b3387ce41cc4723b2d0e144e2
Everyday4106 <167035046+Everyday4106@users.noreply.github.com>
2024-11-06 19:05:17 -0500

    Update coordinator.py

--- custom_components/windmill_fan/coordinator.py
+++ custom_components/windmill_fan/coordinator.py
@@ -1,4 +1,3 @@
-#coordinator.py
 import logging
 from datetime import timedelta
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
@@ -7,11 +6,9 @@ from .const import DOMAIN, UPDATE_INTERVAL
 _LOGGER = logging.getLogger(__name__)
 
 class WindmillDataUpdateCoordinator(DataUpdateCoordinator):
-    """Class to manage fetching data from the Windmill AC API."""
+    """Class to manage fetching data from the Windmill API."""
 
-    def __init__(self, hass, blynk_service):
-        """Initialize."""
-        _LOGGER.debug("2Starting data from Windmill AC")
+    def __init__(self, hass, blynk_service):)
         self.blynk_service = blynk_service
         super().__init__(
             hass,
@@ -21,17 +18,14 @@ class WindmillDataUpdateCoordinator(DataUpdateCoordinator):
         )
 
     async def _async_update_data(self):
-        """Fetch data from Windmill AC."""
-        _LOGGER.debug("Fetching data from Windmill AC")
+        _LOGGER.debug("Fetching data from Windmill")
         try:
             data = {
-                "current_temp": await self.blynk_service.async_get_pin_value('V1'),
-                "target_temp": await self.blynk_service.async_get_pin_value('V2'),
-                "mode": await self.blynk_service.async_get_mode(),
-                "fan": await self.blynk_service.async_get_fan(),
                 "power": await self.blynk_service.async_get_power(),
+                "autofade": await self.blynk_service.async_get_autofade(),
+                "speed": await self.blynk_service.async_get_speed()
             }
-            _LOGGER.debug(f"Data fetched from Windmill AC: {data}")
+            _LOGGER.debug(f"Data fetched from Windmill: {data}")
             return data
         except Exception as err:
             _LOGGER.error(f"Error fetching data: {err}")
