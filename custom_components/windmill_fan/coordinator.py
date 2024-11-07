297c83acce37f8571ba1922a3dbc64bf1a3c03ff
Everyday4106 <167035046+Everyday4106@users.noreply.github.com>
2024-11-07 07:36:02 -0500

    Update coordinator.py

--- custom_components/windmill_fan/coordinator.py
+++ custom_components/windmill_fan/coordinator.py
@@ -1,3 +1,4 @@
+#coordinator.py
 import logging
 from datetime import timedelta
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
@@ -6,9 +7,11 @@ from .const import DOMAIN, UPDATE_INTERVAL
 _LOGGER = logging.getLogger(__name__)
 
 class WindmillDataUpdateCoordinator(DataUpdateCoordinator):
-    """Class to manage fetching data from the Windmill API."""
+    """Class to manage fetching data from the Windmill AC API."""
 
     def __init__(self, hass, blynk_service):
+        """Initialize."""
+        _LOGGER.debug("2Starting data from Windmill AC")
         self.blynk_service = blynk_service
         super().__init__(
             hass,
@@ -18,14 +21,18 @@ class WindmillDataUpdateCoordinator(DataUpdateCoordinator):
         )
 
     async def _async_update_data(self):
-        _LOGGER.debug("Fetching data from Windmill")
+        """Fetch data from Windmill AC."""
+        _LOGGER.debug("Fetching data from Windmill AC")
         try:
             data = {
+                "current_temp": await self.blynk_service.async_get_pin_value('V1'),
+                "target_temp": await self.blynk_service.async_get_pin_value('V2'),
+                "mode": await self.blynk_service.async_get_mode(),
+                "fan": await self.blynk_service.async_get_fan(),
                 "power": await self.blynk_service.async_get_power(),
-                "autofade": await self.blynk_service.async_get_autofade(),
-                "speed": await self.blynk_service.async_get_speed(),
             }
-            _LOGGER.debug(f"Data fetched from Windmill: {data}")
+            _LOGGER.debug(f"Data fetched from Windmill AC: {data}")
             return data
         except Exception as err:
+            _LOGGER.error(f"Error fetching data: {err}")
             raise UpdateFailed(f"Error fetching data: {err}")
