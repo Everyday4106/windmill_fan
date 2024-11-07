ac5606969c2d5fc5cfb9e2add98d48e921f06724
Everyday4106 <167035046+Everyday4106@users.noreply.github.com>
2024-11-07 11:45:08 -0500

    Update __init__.py

--- custom_components/windmill_fan/__init__.py
+++ custom_components/windmill_fan/__init__.py
@@ -15,14 +15,10 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     token = entry.data[CONF_TOKEN]
 
     blynk_service = BlynkService(hass, server, token)
-    _LOGGER.debug("1")
     coordinator = WindmillDataUpdateCoordinator(hass, blynk_service)
-    _LOGGER.debug("2")
 
     hass.data.setdefault(DOMAIN, {})
-    _LOGGER.debug("3")
     hass.data[DOMAIN]["coordinator"] = coordinator
-    _LOGGER.debug("4")
 
     await coordinator.async_config_entry_first_refresh()
 
