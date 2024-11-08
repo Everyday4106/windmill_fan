a5ffb55ca2426db36f86d6f490c4806608da010e
Everyday4106 <167035046+Everyday4106@users.noreply.github.com>
2024-11-07 19:43:36 -0500

    Update fan.py

--- custom_components/windmill_fan/fan.py
+++ custom_components/windmill_fan/fan.py
@@ -1,32 +1,36 @@
-class WindmillFan(CoordinatorEntity, LightEntity):
-    """An entity using CoordinatorEntity.
+import logging
+from homeassistant.components.fan import FanEntity, FanEntityDescription
 
-    The CoordinatorEntity class provides:
-      should_poll
-      async_update
-      async_added_to_hass
-      available
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+from homeassistant.helpers.entity import DeviceInfo
 
-    """
+from .const import DOMAIN
+from .blynk_service import BlynkService
+from .coordinator import WindmillDataUpdateCoordinator
 
-    def __init__(self, coordinator, idx):
-        """Pass coordinator to CoordinatorEntity."""
-        super().__init__(coordinator, context=idx)
-        self.idx = idx
+_LOGGER = logging.getLogger(__name__)
 
-    @callback
-    def _handle_coordinator_update(self) -> None:
-        """Handle updated data from the coordinator."""
-        self._attr_is_on = self.coordinator.data[self.idx]["state"]
-        self.async_write_ha_state()
-
-    async def async_turn_on(self, **kwargs):
-        """Turn the light on.
-
-        Example method how to request data updates.
-        """
-        # Do the turning on.
-        # ...
-
-        # Update the data
-        await self.coordinator.async_request_refresh()
+class WindmillFan(CoordinatorEntity, FanEntity):
+    """Representation of a Windmill Fan"""
+    
+    def __init__(self, coordinator, entity_description: FanEntityDescription):
+        """Initialize the climate device."""
+        super().__init__(coordinator)
+        self.entity_description = entity_description
+        self._attr_name = "Windmill Fan"
+        self._attr_unique_id = f"{DOMAIN}_{entity_description.key}"
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, self.unique_id)},
+            name= "Windmill Fan",
+            manufacturer="Windmill"
+        )
+        self._attr_supported_features = (
+            ClimateEntityFeature.SET_SPEED |
+            ClimateEntityFeature.TURN_ON |
+            ClimateEntityFeature.TURN_OFF
+        )
+        self._is_on = False
+        _LOGGER.debug(f"Setup WindmillFan entity: {self.entity_description.name}")
