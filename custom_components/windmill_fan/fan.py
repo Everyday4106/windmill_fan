a6c1a12e095cdbc732bb6d332d87718fce213d27
Everyday4106 <167035046+Everyday4106@users.noreply.github.com>
2024-11-08 08:29:26 -0500

    Update fan.py

--- custom_components/windmill_fan/fan.py
+++ custom_components/windmill_fan/fan.py
@@ -13,32 +13,38 @@ from .coordinator import WindmillDataUpdateCoordinator
 
 _LOGGER = logging.getLogger(__name__)
 
-class WindmillFan(CoordinatorEntity, FanEntity):
+class WindmillFan(FanEntity):
     """Representation of a Windmill Fan"""
     
     def __init__(self, coordinator):
         """Initialize the fan device."""
         _LOGGER.debug("yo")
-        super().__init__(coordinator)
+        _LOGGER.debug(coordinator.data)
         self._attr_name = "Windmill Fan"
+
+        #homeassistant.helpers.device_registry.format_mac
+        #find the MAC via a "discovery handler"
+        self._attr_unique_id = "123456"
         #self._attr_unique_id = f"{DOMAIN}_{entity_description.key}"
         self._attr_device_info = DeviceInfo(
             identifiers={(DOMAIN, self.unique_id)},
             name= "Windmill Fan",
             manufacturer="Windmill"
         )
+        _LOGGER.debug("yo2")
         self._attr_supported_features = (
             FanEntityFeature.SET_SPEED |
             FanEntityFeature.TURN_ON |
             FanEntityFeature.TURN_OFF
         )
+        _LOGGER.debug("yo3")
         self._is_on = False
         #self._speed_count = 5
         #self._autofade = True
         #self._speed = 1
         #percentage	int | None	0	The current speed percentage. Must be a value between 0 (off) and 100.
         #_LOGGER.debug(f"Setup WindmillFan entity: {self.entity_description.name}")
-        _LOGGER.debug(coordinator.data)
+        
 
 
 
