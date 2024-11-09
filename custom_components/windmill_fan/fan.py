5c915033a6dc4aee847cd702234439658236e498
Everyday4106 <167035046+Everyday4106@users.noreply.github.com>
2024-11-09 07:03:29 -0500

    Update fan.py

--- custom_components/windmill_fan/fan.py
+++ custom_components/windmill_fan/fan.py
@@ -1,8 +1,12 @@
+import logging
 from homeassistant.components.fan import FanEntity
+
+_LOGGER = logging.getLogger(__name__)
 
 class WindmillFan(FanEntity):
     _attr_has_entity_name = True
 
     def __init__(self, coordinator):
+        _LOGGER.debug("yo")
         self._is_on = False
-        
+        _LOGGER.debug(coordinator.data)
