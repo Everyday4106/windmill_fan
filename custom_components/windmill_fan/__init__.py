9f4d093484da7f6b7b04ced28588563e7b96bf80
Everyday4106 <167035046+Everyday4106@users.noreply.github.com>
2024-11-08 20:07:45 -0500

    Update __init__.py

--- custom_components/windmill_fan/__init__.py
+++ custom_components/windmill_fan/__init__.py
@@ -32,7 +32,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     fan_entity = WindmillFan(coordinator)
     
     # add the Windmill Fan entity to Home Assistant
-    #add_entities([MyEntity()], update_before_add=True)
+    add_entities([fan_entity], update_before_add=True)
 
     return True
 
