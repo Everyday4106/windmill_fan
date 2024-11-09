f95df5ea2da8d7b90db69a7840f7d642c5dfbd98
Everyday4106 <167035046+Everyday4106@users.noreply.github.com>
2024-11-09 06:39:53 -0500

    Update fan.py

--- custom_components/windmill_fan/fan.py
+++ custom_components/windmill_fan/fan.py
@@ -1,15 +1,12 @@
 import logging
-from homeassistant.components.fan import FanEntity, FanEntityFeature
-
-from .const import DOMAIN
-from .coordinator import WindmillDataUpdateCoordinator
+from homeassistant.components.fan import FanEntity
 
 _LOGGER = logging.getLogger(__name__)
 
 class WindmillFan(FanEntity):
     """Representation of a Windmill Fan"""
   
-      def __init__(self, coordinator):
+      def __init__(self):
         """Initialize the fan device."""
         _LOGGER.debug("yo")
         
