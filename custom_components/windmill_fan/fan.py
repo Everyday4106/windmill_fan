99760de41cd58650ffe97c566969ed226a6b8a1a
Everyday4106 <167035046+Everyday4106@users.noreply.github.com>
2024-11-09 06:36:14 -0500

    Update fan.py

--- custom_components/windmill_fan/fan.py
+++ custom_components/windmill_fan/fan.py
@@ -1,14 +1,7 @@
 import logging
 from homeassistant.components.fan import FanEntity, FanEntityFeature
 
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.update_coordinator import CoordinatorEntity
-from homeassistant.helpers.entity import DeviceInfo
-
 from .const import DOMAIN
-from .blynk_service import BlynkService
 from .coordinator import WindmillDataUpdateCoordinator
 
 _LOGGER = logging.getLogger(__name__)
