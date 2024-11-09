819dcc15cae6b38ef2e056caba3a6215db73779f
Everyday4106 <167035046+Everyday4106@users.noreply.github.com>
2024-11-09 06:57:17 -0500

    Update fan.py

--- custom_components/windmill_fan/fan.py
+++ custom_components/windmill_fan/fan.py
@@ -3,6 +3,6 @@ from homeassistant.components.fan import FanEntity
 class WindmillFan(FanEntity):
     _attr_has_entity_name = True
 
-    def __init__(self):
+    def __init__(self, coordinator):
         self._is_on = False
         #test
