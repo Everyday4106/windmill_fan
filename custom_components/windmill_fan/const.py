1348be44036a8c202a5e2e2d3de666489028d689
Everyday4106 <167035046+Everyday4106@users.noreply.github.com>
2024-11-15 07:03:25 -0500

    Update const.py

--- custom_components/windmill_fan/const.py
+++ custom_components/windmill_fan/const.py
@@ -6,7 +6,6 @@ LOGGER: Logger = getLogger(__package__)
 NAME = "Windmill Fan"
 DOMAIN = "windmill_fan"
 VERSION = "1.0.0"
-PLATFORMS = ["fan"]
 UPDATE_INTERVAL = 60
 CONF_TOKEN = "token"
 CONF_TITLE = "Windmill fan auth token"
